1bbed6c09395d9855f17341e5aa50aa11c181228
ShinNationalKid <heliogabalusroses@gmail.com>
2024-09-05 19:40:30 -0300

    Update prefab player

---
+++ Assets/Resources/ShadersEffects/FogScroll.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogScroll : MonoBehaviour
+{
+    [SerializeField]
+    Camera _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
